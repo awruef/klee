066f4f719c88438005f5538f7fd4b9f9f35888d0
Andrew Ruef <awruef@cs.umd.edu>
2018-06-25 13:11:47 -0400

    .

--- test/Replay/replay_concretize.c
+++ test/Replay/replay_concretize.c
@@ -1,26 +1,21 @@
 // RUN: %llvmgcc %s -emit-llvm -g -O0 -c -o %t.bc
-// RUN: klee --seed-out=%S/replay_concretize_testcase.ktest --only-replay-seeds -posix-runtime %t.bc --sym-stdin 7
+// RUN: klee --seed-out=./replay_concretize_testcase.ktest --only-replay-seeds --write-smt2s -libc=uclibc -posix-runtime ./%t.bc --sym-stdin 7
 
 #include <string.h>
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "klee/klee.h"
 
 int main(int argc, char *argv[]) {
   int blah = 0;
   int bar = 0;
-  char ar[7];
-  klee_make_symbolic(ar, 7, "stdin");
-  char c = ar[0];
+  char c = getchar(); 
   if (c == EOF)
     return 1;
 
   char *d = malloc(c+1);
   memset(d, 0, c+1);
-  int i = 0;
-  while (i < 7) {
-    c = ar[i];
+  while ((c = getchar()) != EOF) {
     d[blah] = c;
     blah++;
   }
